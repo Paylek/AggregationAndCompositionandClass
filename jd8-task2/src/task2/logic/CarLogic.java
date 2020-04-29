6be75c9ec26c294d6eed07eb45c1a5e021c57f4b
User <User@PC>
2020-04-29 16:45:55 +0300

    .

---
+++ jd8-task2/src/task2/logic/CarLogic.java
@@ -0,0 +1,45 @@
+package task2.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import task2.bean.Car;
+import task2.bean.Wheel;
+
+public class CarLogic {
+	public void drive(Car car) {
+
+		if (car.getEngine() != null) {
+			if (car.getFueltank() == 0) {
+				System.out.println("Заправьте автомобиль");
+
+			} else {
+				System.out.println("Машина поехала");
+			}
+		} else {
+			System.out.println("У вас нет двигателя");
+		}
+
+	}
+
+	public Car refuel(Car car, int fuel) {
+
+		car.setFueltank(fuel);
+		
+		System.out.println("Машина заправлена");
+
+		return car;
+	}
+
+	public Car changeWheels(Car car, int numberOfWheel, Wheel newWheel) {
+
+		List<Wheel> listWheels = new ArrayList<Wheel>(car.getWheels());
+
+		listWheels.set(numberOfWheel, newWheel);
+
+		car.setWheels(listWheels);
+
+		return car;
+	}
+
+}
---
+++ jd8-task2/src/task2/view/ViewCarMake.java
@@ -0,0 +1,11 @@
+package task2.view;
+
+import task2.bean.Car;
+
+public class ViewCarMake {
+public void viewCarMake(Car car) {
+		
+		System.out.println("Марка автомобиля " + car.getCarMake());
+		
+	}
+}
